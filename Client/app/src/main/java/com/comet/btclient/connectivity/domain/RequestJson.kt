ed4f6a213d0fd11c8dc4b22e45709589b9d774ec
choi-hyeseong <bd2845@kakao.com>
2024-08-16 14:53:14 +0900

    feat: support null body 단순한 url html 호출시에는 body 필요 없음

--- Client/app/src/main/java/com/comet/btclient/connectivity/domain/RequestJson.kt
+++ Client/app/src/main/java/com/comet/btclient/connectivity/domain/RequestJson.kt
@@ -1,4 +1,4 @@
 package com.comet.btclient.connectivity.domain
 
 // 웹 요청시 전송되는 json입니다.
-data class RequestJson (val url : String, val contentType : String, val body : String)
+data class RequestJson (val url : String, val contentType : String, val body : String?)
